c15507a6ea8f507b011d48c23ceff6369cccd557
Olivier Moitroux <o.moitroux@student.ulg.ac.be>
2019-07-03 12:47:31 +0200

    shortcuts, home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:phoenix/utils/colors.dart';
 
-/// Simple text page
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  final String title;
 
+  HomeScreen({Key key, @required this.title}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Lorem Ipsum"),
+          title: Text(widget.title),
           backgroundColor: covoitULiegeColor,
         ),
         body: new Container(
             padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
             child: new Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum")));
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco "
+                    "laboris nisi ut aliquip ex ea commodo consequat. "
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse "
+                    "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat "
+                    "cupidatat non proident, sunt in culpa qui officia deserunt "
+                    "mollit anim id est laborum"
+            )
+        )
+    );
   }
 }
---
+++ lib/shortcusts.md
@@ -0,0 +1,32 @@
+# Useful shortcuts
+
+## Snippet
+* **ctrl + j**: insert live template
+* **alt + enter**: surround with
+* **alt + insert**: toggle generator
+
+## Editor
+* **ctrl + alt + L**: reformat code 
+* **ctrl + :**: comment selection
+* **middle mouse + vertical drag**: generate multiple prompts
+* **alt + shift + up + arrow**: drag *line* up or down
+* **ctrl+Maj + arrow**: drag *statement* up or down
+* **shift + F6**: refactor
+
+## Text formating
+* **ctrl + x**: cut 
+* **ctrl + D**: duplicate current line & paste to the next one
+
+## Navigation:
+* **ctrl + arrows**: move cursor words by words
+* **ctrl + shift + arrow**: selection word by word
+* **Home/End**: go to begin/end of line
+* **ctrl + home/end**: go to begin/end of file
+
+## Browser
+* **ctrl + shift + t**: open previously closed tab in browser
+
+
+## Others:
+
+[shortcuts for programmers](https://dev.to/designpuddle/27-no-frills-keyboard-shortcuts-every-developer-should-follow-4jd)
